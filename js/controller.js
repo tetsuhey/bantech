edc9e1bc2d34c14a934bbd72d7107a30ba73b057
tetsuhey <tetsu.o.dev@gmail.com>
2021-08-25 15:24:00 +0900

    fist

---
+++ js/controller.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(document).on('menuopen',function(){
+        $('nav .menu').toggleClass('open');
+        $('#menu_btn').toggleClass('open');
+        $('#menu_bk').toggleClass('open');
+    });
+    $('.menu_btn').on('click', function() {
+        $(document).trigger('menuopen');
+    });
+    $('#menu_bk').on('click',function(){
+        $(document).trigger('menuopen');
+    })
+});
